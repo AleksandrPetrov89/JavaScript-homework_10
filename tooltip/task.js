ffe0b35735c3682b18b8d09cb2dffa72ba4ffbce
Петров Александр <petrovav89@gmail.com>
2023-07-23 16:40:06 +0500

    Task 1 (Tooltip). Tooltip positioning fixed

--- tooltip/task.js
+++ tooltip/task.js
@@ -3,7 +3,7 @@ for (let link of links) {
     let hint = document.createElement("div");
     hint.classList.add("tooltip");
     hint.textContent = link.title;
-    hint.dataset.position = "bottom";
+    hint.dataset.position = "right";
     link.append(hint);
     link.addEventListener("click", showHint, true)
 };
@@ -19,4 +19,22 @@ function showHint (e) {
         };
     };
     hint.classList.toggle("tooltip_active");
+    if (hint.classList.contains("tooltip_active")) {
+        domRect= link.getBoundingClientRect();
+        if (hint.dataset.position == "bottom" || hint.dataset.position == "top") {
+            hint.style.left = `${domRect.left}px`;
+            if (hint.dataset.position == "bottom") {
+                hint.style.top = `${domRect.bottom}px`;
+            } else {
+                hint.style.top = `${domRect.top - hint.offsetHeight}px`;
+            };    
+        } else if (hint.dataset.position == "left" || hint.dataset.position == "right") {
+            hint.style.top = `${domRect.top}px`;
+            if (hint.dataset.position == "left") {
+                hint.style.left = `${domRect.left - hint.offsetWidth}px`;
+            } else {
+                hint.style.left = `${domRect.right}px`;
+            };
+        };
+    };
 };
